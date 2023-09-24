19d392d54e77435963cad2b8dfd157e5dd2b386b
Eduardo Tai <eduardotaidsr@gmail.com>
2023-09-24 03:41:35 -0300

    player1 and player2 now can play together

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,35 @@
 const gameBoard = (function() {
     const displayTheBoard = document.getElementById('ticTacToeBoard');
     let board = ['','','','','','','','',''];
-
-
-
+    
     board.forEach((value, item) => {
-            let div = document.createElement('div');
-                div.classList.add('grid');
-                div.textContent = value;
-                displayTheBoard.appendChild(div);
-    });
-})();
+      let div = document.createElement('div');
+      div.classList.add('grid');
+      div.textContent = value;
+      displayTheBoard.appendChild(div);
+    });    
+    
+    let currentPlayer = 'X';
 
-
-function playerChoose() {
-    const playerMarker = (marker) => {
-        marker = '';
-    }
-
-    const playerMarker2 = (marker) => {
-        marker = '';
-    }
-
- 
-    const playerPrimary = document.getElementById('player1')
-    playerPrimary.addEventListener('click', () => {
-        const player1 = prompt('Player 1 Choose your symbol: X or O');
-        if(player1 === 'X') {
-            playerMarker.marker = 'X';
-        } else {
-            playerMarker2.marker = 'O';
-        }        
-    })
-    const grid = document.querySelectorAll('.grid');
-    grid.forEach((value, item) => {
-        value.addEventListener('click', () => {
-            if (value.textContent === '') {
-                value.textContent = playerMarker.marker;
-            } 
+    const playerMove = function() {
+      const grid = document.querySelectorAll('.grid');
+      grid.forEach((cell) => {
+        cell.addEventListener('click', () => {
+          if (cell.textContent === '') {
+            cell.textContent = currentPlayer;
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+          }
         });
-    });
-}
-
-playerChoose();
-
-
-
-
+      });
+    }
+    
+    const init = function() {
+      playerMove();
+    }
+    return {
+      init: init
+    }
+  })();
+  
+  gameBoard.init();
+  
